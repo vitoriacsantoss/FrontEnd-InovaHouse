3413d0a4ec23a9661ee49c5a556ff82515c0c116
asantosmanu <manu452006@gmail.com>
2024-10-30 16:45:46 -0300

    destaques prontos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f081bd2og1"
+    "revision": "0.j1sf9a2ptmg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/ImovelList.vue
+++ src/components/ImovelList.vue
@@ -150,6 +150,33 @@ onMounted(async () => {
 
 
 <script setup>
+import { onMounted, watch } from 'vue';
+import { useImoveisStore } from '@/stores/imovel';
+
+import { formatDescription, formatPrice, formatTitle } from '@/helpers/format';
+
+const props = defineProps(['category_id']);
+const imoveisStore = useImoveisStore();
+
+async function getImoveis() {
+  if (props.category_id) {
+    await imoveisStore.getImovelByCategory(props.category_id);
+  } else {
+    await imoveisStore.getImoveis();
+  }
+}
+
+watch(
+  () => props.category_id,
+  async () => {
+    await getImoveis();
+  },
+);
+
+onMounted(async () => {
+  await getImoveis();
+  console.log(getImoveis.value)
+});
 // import DestaqueView from '@/components/DestaqueView.vue'
 import { ref } from 'vue'
 const estado = ref('')
@@ -214,6 +241,44 @@ const estado = ref('')
   <div class="destaques">
     <destaque-view />
   </div>
+
+<div class="list"></div>
+<hr>
+<div class="imovel-list">
+    <router-link :to="{ name: 'ImovelAdd' }">
+      <button class="icon ">
+        <i class="mdi mdi-plus" />
+      </button>
+    </router-link>
+    <div v-if="imoveisStore.imoveis.length === 0">
+      <p>Produtos não encontrados!!!</p>
+    </div>
+    <div v-for="imovel in imoveisStore.imoveis" :key="imovel.id" class="imovel-card">
+      <div class="imovel-img-wrapper">
+        <img :src="imovel.foto?.url" alt="imovel.name" />
+        <i class="mdi mdi-heart-outline" />
+      </div>
+      
+      <div class="imovel-title-price">
+        <p> 
+          {{ formatPrice(imovel.preco * 1) }}</p>
+      </div>
+      <div class="imovel-title-price">
+        <p>{{ (imovel.localizacao) }}</p>
+      </div>
+      
+      <div class="imovel-description-stars">
+        <div class="stars">
+          <i class="mdi mdi-star" />
+          <i class="mdi mdi-star" />
+          <i class="mdi mdi-star" />
+          <i class="mdi mdi-star" />
+          <i class="mdi mdi-star" />
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
@@ -329,6 +394,87 @@ const estado = ref('')
   padding-top: 200px;
   padding-bottom: 200px;
   background-size: cover;
+}
+
+
+.icon {
+  background-color: #0a2668;
+  color: white;
+  border: none;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+
+  position: fixed;
+  bottom: 12rem;
+  right: 20px;
+}
+
+.icon:hover {
+  background-color: #bac9e8;
+  color: #0a2668;
+}
+
+.icon i {
+  font-size: 2rem;
+}
+
+.imovel-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1.5rem;
+  padding: 1rem;
+}
+
+.imovel-card {
+  margin-bottom: 30px;
+  margin-top: 50px;
+  width: 225px;
+  font-family: 'Belleza', sans-serif;
+}
+
+
+.imovel-img-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: top;
+  gap: 0.5rem;
+  padding-top: 20px;
+  margin-bottom: 1rem;
+  background-color: #eeeeee;
+  height: 201px;
+}
+
+.imovel-img-wrapper img {
+  width: 153px;
+  height: 170px;
+  object-fit: cover;
+}
+
+.imovel-title-price {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+
+.imovel-title-price p {
+  font-weight: bold;
+  font-size: 16px;
+  color: #010101;
+}
+
+.imovel-description-stars {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+}
+
+.imovel-description-stars p {
+  font-size: 12px;
+  color: #535050;
 }
 </style>
 
